6ebf85cc2f7527c6be3c696d3cd078787e0dbe2a
Daniel Stoker <eighty3labels@yahoo.com>
2025-02-09 20:19:49 +0100

    Practice with desctructuring and scope.

---
+++ variables.js
@@ -0,0 +1,11 @@
+// DESTRUCTURING
+
+// Array destructuring
+const arr = [1, 2, 3];
+const [a, b, c] = arr;
+console.log(a, b, c); // 1, 2, 3
+
+// Object destructuring
+const obj = { name: 'John', age: 30 };
+const { name, age } = obj;
+console.log(name, age); // John, 30
